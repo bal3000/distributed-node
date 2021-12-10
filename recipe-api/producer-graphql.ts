24b6c4f935cadead11476bbde582da376a81928c
bal3000 <bal3000286@googlemail.com>
2021-12-10 13:30:22 +0000

    graphql added

--- recipe-api/producer-graphql.ts
+++ recipe-api/producer-graphql.ts
@@ -1,38 +1,35 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import Fastify from 'fastify';
 import graphql from 'fastify-gql';
 
-const server = Fastify({});
+const server = Fastify({ logger: true });
 
-const schema = fs
-  .readFileSync(`${__dirname}/../shared/graphql-schema.gql`)
-  .toString();
+const schema = readFileSync(
+  __dirname + '/../shared/graphql-schema.gql'
+).toString();
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 4000;
 
 const resolvers = {
   Query: {
     pid: () => process.pid,
-    recipe: async (_obj: any, { id }: { id: number }) => {
-      if (id != 42) {
-        throw new Error(`recipe ${id} not found`);
-      }
-
+    recipe: async (_obj: any, { id }: any) => {
+      if (id != 42) throw new Error(`recipe ${id} not found`);
       return {
         id,
         name: 'Chicken Tikka Masala',
         steps: 'Throw it in a pot...',
       };
     },
-    Recipe: {
-      ingredients: async (obj: any) => {
-        return obj.id != 42
-          ? []
-          : [
-              { id: 1, name: 'Chicken', quantity: '1 lb' },
-              { id: 2, name: 'Sauce', quantity: '2 cups' },
-            ];
-      },
+  },
+  Recipe: {
+    ingredients: async (obj: any) => {
+      return obj.id != 42
+        ? []
+        : [
+            { id: 1, name: 'Chicken', quantity: '1 lb' },
+            { id: 2, name: 'Sauce', quantity: '2 cups' },
+          ];
     },
   },
 };
